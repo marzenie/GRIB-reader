9475f567df8b4dcc10a3f229fb15c70642eeb53c
Marzycielx <marszalek_szymon@tlen.pl>
2024-05-08 15:31:28 +0200

    fix scan mode

--- main.cpp
+++ main.cpp
@@ -417,7 +417,7 @@ std::cout << R"(
 
 	for (int i = 1; i <= num_points_along_longitude_meridian; i++) {
 		binary = "";
-		for (int i = 0; i < 2; ++i) {
+		for (int j = 0; j < 2; ++j) {
 			grib.read(&byte_char, 1);
 			binary += std::bitset<8>((int)byte_char).to_string();
 		}
